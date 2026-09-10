d365086faba03011162d03fdd01921b3a4c730d6
Zeward <82192948+zeward-lee@users.noreply.github.com>
2026-09-10 13:14:36 +0800

    chore: change applicationId to match fxliang for overwrite install

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,7 @@ android {
         }
         create(flavorMainline) {
             dimension = "brand"
+            applicationId = "org.fcitx.fcitx5.android.fx"
             buildConfigField("boolean", "IS_FX_BUILD", "false")
         }
     }
