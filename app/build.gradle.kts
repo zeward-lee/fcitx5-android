0b753c8400435c9a87898443cbbc7c3591dd7f2e
Zeward <82192948+zeward-lee@users.noreply.github.com>
2026-09-10 14:37:07 +0800

    chore: limit apk to arm64-v8a only to reduce size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,10 @@ android {
         buildConfigField("String", "ORIGINAL_DEBUG_PLUGIN_MANIFEST_ACTION", "\"$originalDebugPluginManifestAction\"")
         resValue("string", "ime_settings_activity", imeSettingsActivity)
 
+        ndk {
+            abiFilters += "arm64-v8a"
+        }
+
         @Suppress("UnstableApiUsage")
         externalNativeBuild {
             cmake {
